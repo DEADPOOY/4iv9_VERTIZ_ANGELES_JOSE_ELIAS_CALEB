109b2dec92dd2022cdbce1d7ce7b9831925df57c
DEADPOOY <jvertiza2400@alumno.ipn.mx>
2026-03-14 18:17:16 -0600

    capturas de seguimiento problemas java

--- 02EntradaDatos/EntradaTexto.java
+++ 02EntradaDatos/EntradaTexto.java
@@ -7,17 +7,17 @@ class EntradaTexto {
         //instanciamos el objeto
         Scanner entrada = new Scanner(System.in);
        //las variables locales
-       String nombre;
-       int edad;
+        String nombre;
+        int edad;
        //entrada
-       System.out.println("escribe tu nombre");
-       nombre= System.console().readLine();
+        System.out.println("escribe tu nombre");
+        nombre= System.console().readLine();
 
-       System.out.println("hola " + nombre + " bienvenido a programar my feo");
+        System.out.println("hola " + nombre + " bienvenido a programar my feo");
 
-       System.out.println("Escribe tu edad: ");
-       edad = entrada.nextInt();
-       System.out.println("hola " + nombre +" tu edad es de "+ edad);
+        System.out.println("Escribe tu edad: ");
+        edad = entrada.nextInt();
+        System.out.println("hola " + nombre +" tu edad es de "+ edad);
     }
     
 }
