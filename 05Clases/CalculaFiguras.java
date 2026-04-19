305de49f385a3556da861adf5b6ed4ec86edee98
DEADPOOY <jvertiza2400@alumno.ipn.mx>
2026-04-18 20:44:13 -0600

    modificando el codigo pa entenderle chido pq me falta la interfaz zzzz

--- 05Clases/CalculaFiguras.java
+++ 05Clases/CalculaFiguras.java
@@ -4,6 +4,8 @@ public class CalculaFiguras{
     //wuachese profe el mejor codigo de mi vida ahora evite el copiar wazza👻👻
     Scanner entrada = new Scanner(System.in);
     public void menu(){
+        int opcionMenu;
+        do{
         //vamos a colocar 5 figuras en el menu y va a ser de ahi un chuich
         //triangulo, cuadrado, trapecio, circulo, rombo
         System.out.println("\nIngresa la opcion que deseas usar");
@@ -13,9 +15,9 @@ public class CalculaFiguras{
         System.out.println("||3.-Triangulo                    ||");
         System.out.println("||4.-Trapecio                     ||");
         System.out.println("||5.-Rombo                        ||");
+        System.out.println("||6.-Salir                        ||");
         System.out.println("||================================||");
         System.out.println("\n");
-        int opcionMenu = 0;
         opcionMenu = entrada.nextInt();
         switch (opcionMenu) {
             //mire profe aqui no recuerdo muy bien pero en progra basica enseñaron clases asi
@@ -39,61 +41,61 @@ public class CalculaFiguras{
                 System.out.println("opcion invalida intenta de nuevo");
                 break;
         }
+        }while (opcionMenu != 0);
     }
     //si funciono na la mera verdura del caldo
     //es mas profe yo doy la clase 👻👻, nah mentira
     public void CalcularCirculo(){
         System.out.println("\nintroduce el radio del circulo");
-        float radio = entrada.nextFloat();
-        double diametro = radio*2;
-        double Area = Math.PI * radio * radio;//no supe como meter el cuadrado xd
-        double Perimetro = Math.PI * diametro;
+        float r = entrada.nextFloat();
+        double di = r*2;
+        double Area = Math.PI * r * r;//no supe como meter el cuadrado xd
+        double Perimetro = Math.PI * di;
         System.out.println("\nEl area del circulo es de: " + Area);
         System.out.println("El perimetro del circulo es: " + Perimetro);
     }
     public void CalcularCuadrado(){
         System.out.println("\nintroduce el lado del cuadrado");
-        float lado = entrada.nextFloat();
-        double Area = lado * lado;
-        double Perimetro = lado*4;
+        float L = entrada.nextFloat();
+        double Area = L * L;
+        double Perimetro = L*4;
         System.out.println("\nEl area del cuadrado es: " + Area);
         System.out.println("El perimetro del cuadrado es: " + Perimetro);
     }
     public void CalcularTriangulo(){
         System.out.println("\nintroduce la base del triangulo");
-        float base = entrada.nextFloat();
+        float b = entrada.nextFloat();
         System.out.println("\nintroduce la altura");
-        float altura = entrada.nextFloat();
-        double Area = (base*altura)/2;
-        double Perimetro = base + base + base;
+        float h = entrada.nextFloat();
+        double Area = (b*h)/2;
+        double Perimetro = b + b + b;
         System.out.println("\nEl area del triangulo es: " + Area);
         System.out.println("El perimetro del triangulo es: " + Perimetro);
     }
     public void CalcularTrapecio(){
         System.out.println("\ningresa base mayor");
-        float BaseM = entrada.nextFloat();
+        float BaMa = entrada.nextFloat();
         System.out.println("\ningresa base menor");
-        float Basem = entrada.nextFloat();
+        float Bame = entrada.nextFloat();
         System.out.println("\ningreasa la altura");
-        float altura = entrada.nextFloat();
+        float h = entrada.nextFloat();
         System.out.println("\ningresa el lado");
         float lado = entrada.nextFloat();
-        double Area = ((BaseM + Basem)*altura)/2;
-        double Perimetro = BaseM + Basem + (2*lado);
+        double Area = ((BaMa + Bame)*h)/2;
+        double Perimetro = BaMa + Bame + (2*lado);
         System.out.println("\nEl area del trapecio es: " + Area);
         System.out.println("El area del trapecio es: " + Perimetro);
     }
     public void CalcularRombo(){
         System.out.println("\ningresa la diagonal mayor");
-        float DiagonalM = entrada.nextFloat();
+        float DiMa = entrada.nextFloat();
         System.out.println("\ningresa la diagonal menor");
-        float Diagonalm = entrada.nextFloat();
+        float Dime = entrada.nextFloat();
         System.out.println("\ningresa un lado");
         Float lado = entrada.nextFloat();
-        double Area = (DiagonalM * Diagonalm)/2;
+        double Area = (DiMa * Dime)/2;
         double Perimetro = 4 * lado;
         System.out.println("\nEl area del rombo es: " + Area);
         System.out.println("El perimetro del rombo es: " + Perimetro);
-        return;
     }
 }
