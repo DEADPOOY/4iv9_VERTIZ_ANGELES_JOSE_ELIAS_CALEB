1144589c1c27a1812338a4c58c81f435d6c6ed5c
DEADPOOY <jvertiza2400@alumno.ipn.mx>
2026-05-10 12:07:47 -0600

    codigo de la clase

--- 08HerenciasPersona/src/main/java/herenciaspersona/HerenciaPersona.java
+++ 08HerenciasPersona/src/main/java/herenciaspersona/HerenciaPersona.java
@@ -6,8 +6,12 @@ package herenciaspersona;
 
 /**
  *
- * @author Alumno
+ * @author DEADPOOY2
  */
-public class estudiante {
-    
+public class HerenciaPersona {
+
+    public static void main(String[] args) {
+        DAOEstudiante objmenu = new DAOEstudiante();
+        objmenu.menu();
+    }
 }
