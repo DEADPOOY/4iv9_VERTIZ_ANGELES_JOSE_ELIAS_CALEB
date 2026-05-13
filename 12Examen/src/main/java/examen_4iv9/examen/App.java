1645e96a72a167cfbe563611676f55cec2af361d
DEADPOOY <jvertiza2400@alumno.ipn.mx>
2026-05-13 09:09:47 -0600

    lo que le faltaba a mi git

---
+++ 12Examen/src/main/java/examen_4iv9/examen/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package examen_4iv9.examen;
+
+/**
+ *
+ * @author dell
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
